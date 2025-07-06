4baf348f749d6552774b21d58213226891161f4b
bantee-sharma <banteesharma9@gmail.com>
2025-07-06 15:48:00 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,8 @@
 from flask import Flask,request
+import pickle
+
+file = open("classifier.pkl","rb")
+model = pickle.load(file)
 
 # flask --app hello.py run
 
@@ -13,4 +17,30 @@ def flask():
     if request.method == "GET":
         return "Insurance Prediction"
     else:
-        return "We are gonna make Prediction"
+        loan = request.get_json()
+
+        if loan["Gender"] == "Male":
+            Gender = 0
+        else:
+            Gender = 1
+        
+        if loan['Married'] == "No":
+            Married = 0
+        else:
+            Married = 1
+
+        ApplicantIncome = loan['ApplicantIncome']
+        LoanAmount = loan['LoanAmount']
+        Credit_History= loan['Credit_History'] 
+
+
+        input_data = [Gender, Married, ApplicantIncome, LoanAmount, Credit_History]
+        result = model.predict([input_data])
+
+        if result == 0:
+            pred = "Rejected"
+        else:
+            pred = "Approved"
+
+        return {"loan_approval_status":pred}
+
