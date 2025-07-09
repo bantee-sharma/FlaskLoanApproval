16ed4a193e89c23f49dd8b86bf9a9fd945e5ec98
bantee-sharma <banteesharma9@gmail.com>
2025-07-09 16:43:23 +0530

    Create neeew.py

---
+++ neeew.py
@@ -0,0 +1,11 @@
+from flask import Flask, request
+import pickle
+
+file = open("classifier.pkl","rb")
+model = pickle.load(file)
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello world"
